a39dfb505a69ee0872a8291d573307ad45ca0bfb
aishanjie <injie@sas.upenn.edu>
2021-09-27 05:50:31 -0400

    testing change

--- Assignment 1 Final.R
+++ Assignment 1 Final.R
@@ -20,14 +20,17 @@ class(dat$Murder)
 hist(dat$Murder, main="Histogram of Murder", xlab="Arrests per 100,000 Residents", ylab="Frequency")
 ```
 summary(dat$Murder)
-```{r, eval=TRUE}
+
+```{r, eval=FALSE}
 par(mfrow=c(3,1))
 hist(dat$Murder, main="Histogram of Murder", xlab="Arrests per 100,000 Residents", ylab="Frequency")
 hist(dat$Assault, main="Histogram of Assault", xlab="Arrests per 100,000 Residents", ylab="Frequency")
 hist(dat$Rape, main="Histogram of Rape", xlab="Arrests per 100,000 Residents", ylab="Frequency")
 ```
-
-```{r, eval = FALSE, fig.width = 7.5, fig.height = 4}
+```{r, eval=TRUE}
+?par
+```
+```
 install.packages("maps")
 install.packages("ggplot2")
 
@@ -35,6 +38,11 @@ library(maps)
 library(ggplot2) 
 
 ggplot(dat, aes(map_id=state, fill=Murder)) + 
-  geom_map(map=jmap_data("state")) + 
+  geom_map(map=map_data("state")) + 
   expand_limits(x=map_data("state")$long, y=map_data("state")$lat)
 ```
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(tidy.opts = list(width.cutoff = 60), tidy = TRUE)
+```
+
+  
